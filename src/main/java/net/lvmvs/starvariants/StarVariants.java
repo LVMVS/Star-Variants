14a727d9f2dd9a44e26ad45ee15fc6cf7d7fc780
CLDHero <colbyevans1995@gmail.com>
2025-12-17 12:24:17 -0500

    test

--- src/main/java/net/lvmvs/starvariants/StarVariants.java
+++ src/main/java/net/lvmvs/starvariants/StarVariants.java
@@ -1,5 +1,6 @@
 package net.lvmvs.starvariants;
 
+import net.lvmvs.starvariants.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -49,6 +50,9 @@ public class StarVariants {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -62,7 +66,9 @@ public class StarVariants {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.IRONSC);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
